0ff86e7d2593c975fcc4e48c0655f649355e91de
NadezhdaDX <37904867+NadezhdaBobrovaDX@users.noreply.github.com>
2022-06-20 16:51:56 +0400

    14.1.6 chnages

--- CS/WindowsFormsApplication2/Form1.cs
+++ CS/WindowsFormsApplication2/Form1.cs
@@ -1,25 +1,17 @@
-﻿using DevExpress.Utils.Html;
-using System;
-using System.Drawing;
+﻿using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication2 {
 	public partial class Form1 : Form {
 		public Form1() {
 			InitializeComponent();
-            alertControl2.HtmlImages = svgImageCollection1;
-        }
-
-        protected override void OnShown(EventArgs e)
-        {
-            base.OnShown(e);
-        }
+		}
         private void simpleButton1_Click(object sender, EventArgs e) {
             alertControl1.Show(this, "Caption", "Text", "Hot-tracked text", null, null, colorPickEdit1.Color);
         }
 
         private void btnRegularAlert_Click(object sender, EventArgs e) {
-            alertControl1.Show(this, "Caption", "Text", "Hot-tracked text", (Image)null, null);
+            alertControl1.Show(this, "Caption", "Text", "Hot-tracked text", null, null);
         }
-    }
+	}
 }
